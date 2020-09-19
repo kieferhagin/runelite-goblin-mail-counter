3714ffd37c7a9969ab672cc4c4580ad3def15149
Kiefer Hagin <kieferhagib@gmail.com>
2020-09-19 16:23:34 -0700

    set up

--- src/test/java/com/haginbros/goblinmail/GoblinMailPluginTest.java
+++ src/test/java/com/haginbros/goblinmail/GoblinMailPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.haginbros.goblinmail;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class GoblinMailPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(GoblinMailPlugin.class);
 		RuneLite.main(args);
 	}
 }
